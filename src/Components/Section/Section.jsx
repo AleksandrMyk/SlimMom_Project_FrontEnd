73fee4ee0eb773958d2613423dce9f8bfb8d7f4c
unknown <you@example.com>
2020-11-28 11:08:05 +0200

    fix

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import s from "./Section.module.css";
+import style from "./Section.module.css";
 
 const Section = ({ children }) => {
-  return <div className={s.Section}>{children}</div>;
+  return <div className={style.Section}>{children}</div>;
 };
 
 export default Section;
