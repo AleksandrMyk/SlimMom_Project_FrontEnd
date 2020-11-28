80dd65543cd8e329f005896f419ae2bd3fbfe273
unknown <you@example.com>
2020-11-28 12:24:43 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-// import { Route, Switch } from "react-router-dom";
-// import NavigationBar from "./Components/NavigationBar";
-// import Spiner from "./Components/Spiner";
-import Aaa from "./Components/AddProductForm";
-// import DailyCaloriesForm from "./Components/DailyCaloriesForm";
-// import Register from "./Views/Register";
-// import Login from "./Views/Login";
-import Modal from "./Components/Modal";
+import React, { Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import NavigationBar from "./Components/NavigationBar";
+import Spiner from "./Components/Spiner";
+import DailyCaloriesForm from "./Views/DailyCaloriesForm";
+import Register from "./Views/Register";
+import Login from "./Views/Login";
 import "./app.css";
 
 const App = () => {
   return (
     <>
-      {/* <NavigationBar></NavigationBar>
-      <Suspense fallback={<Spiner />}></Suspense> */}
-      <Aaa></Aaa>
-      <Modal></Modal>
-      {/* <Switch>
+      <NavigationBar></NavigationBar>
+      <Suspense fallback={<Spiner />}></Suspense>
+      <Switch>
         <Route exact path="/" component={DailyCaloriesForm} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-      </Switch> */}
+      </Switch>
     </>
   );
 };
