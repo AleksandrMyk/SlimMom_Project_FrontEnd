953fa51ee320b6e84eb616f594e5466c09eddf71
unknown <you@example.com>
2020-11-28 20:03:42 +0200

    fix spiner

--- src/Components/Spiner/Spiner.js
+++ src/Components/Spiner/Spiner.js
@@ -4,13 +4,15 @@ import style from "./Spiner.module.css";
 
 const Spiner = () => {
   return (
-    <Loader
-      className={style.loader}
-      type="ThreeDots"
-      color="#FC842D"
-      height={100}
-      width={100}
-    />
+    <div className={style.container}>
+      <Loader
+        className={style.loader}
+        type="ThreeDots"
+        color="#FC842D"
+        height={100}
+        width={100}
+      />
+    </div>
   );
 };
 export default Spiner;
