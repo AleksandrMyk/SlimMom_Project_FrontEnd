2039ef55356c37740fe27f6d2de1b8f720b9bb53
hetman07 <pvp071984@gmail.com>
2020-11-29 01:44:35 +0200

    AddProduct action,operation,reducer & my variant Store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -13,3 +13,42 @@ const store = createStore(
 
 export const persistor = persistStore(store);
 export default store;
+
+/**** Миш посмотри на такой вариант Стора*/
+// import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+// import storage from "redux-persist/lib/storage";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import productReducer from "./product/productReducer";
+// import authReducer from "./auth/authReducers";
+
+// const defaultMiddleware = getDefaultMiddleware();
+
+// const authPersistConfig = {
+//   key: "auth",
+//   storage,
+//   whitelist: ["token"],
+// };
+
+// export const store = configureStore({
+//   reducer: {
+//     products: productReducer,
+//     auth: persistReducer(authPersistConfig, authReducer),
+//   },
+//   middleware: getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// });
+
+// export const persistor = persistStore(store);
