132eefc78b66ecfef76c93101029af108a7f7e93
Eueniy <tkachev.eugeny@gmail.com>
2020-11-30 10:20:27 +0200

    Tkachev_diaryPage02

--- src/pages/DiaryPage.jsx
+++ src/pages/DiaryPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import AddProductForm from "./../Components/AddProductForm"
+import DiaryDateCalendar from "./../Components/Calendar/CalendarOnClick"
 
 const DiaryPage = ({ date }) => {
   return (
     <>
       <h1>{date}</h1>
-      <>Календарь</>
+      <DiaryDateCalendar />
       <AddProductForm />
     </>
   );
