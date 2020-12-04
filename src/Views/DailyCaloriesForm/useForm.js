709277d1333aecbe923883f27972d16aab30e516
unknown <you@example.com>
2020-12-04 22:57:52 +0200

    fixed global modal opening

--- src/Views/DailyCaloriesForm/useForm.js
+++ src/Views/DailyCaloriesForm/useForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const useForm = (callback, validate) => {
   const [bludType, setBludType] = useState(null);
@@ -11,21 +11,19 @@ const useForm = (callback, validate) => {
   });
 
   const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors]);
 
   const handleSubmit = (event) => {
     if (event) event.preventDefault();
-    setErrors({
-      ...validate(values),
-    });
-
-    setValues({
-      height: "",
-      age: "",
-      currentWeight: "",
-      targetWeight: "",
-      bloodType: "",
-    });
-    setBludType(null);
+    setErrors(validate(values));
+    setIsSubmitting(true);
+    console.log(values);
   };
 
   const handleChange = (e) => {
@@ -37,8 +35,10 @@ const useForm = (callback, validate) => {
   };
 
   const handleBludChange = (e) => {
-    setBludType(e.target.value);
-    values.bloodType = Number(e.target.value);
+    setValues({
+      ...values,
+      bloodType: Number(e.target.value),
+    });
   };
 
   return {
