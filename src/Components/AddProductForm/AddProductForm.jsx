1f00e00776f19fe3debe8bc0ba02c0dd70bca317
Michael <mykhaylo88@icloud.com>
2020-12-05 09:40:58 +0200

    fix

--- src/Components/AddProductForm/AddProductForm.jsx
+++ src/Components/AddProductForm/AddProductForm.jsx
@@ -1,97 +1,58 @@
-import React, { useState, useCallback } from "react";
-import { useDispatch } from "react-redux";
-import { useForm, useField } from "react-final-form-hooks";
-import { useMediaQuery } from "./hooks";
-import productOperations from "../../Redux/product/productOperations";
-import styles from "./AddProductForm.module.css";
-import Calendar from "../Calendar";
-import DiaryProductList from "../DiaryProductsList";
-
-const AddProductForm = () => {
-  const dispatch = useDispatch();
-  const [isSubmitting, setIsSubmitting] = useState(false);
-
-  const onSubmit = useCallback(
-    (e) => {
-      //e.preventDefault();
-      dispatch(productOperations.addProduct());
-      setIsSubmitting(true);
-      window.alert(JSON.stringify(e, 0, 2));
-    },
-    [dispatch]
-  );
-
-  const validate = (values) => {
-    const errors = {};
-    if (!values.nameProd) {
-      errors.nameProd = "Required";
-    }
-    if (values.nameProd && RegExp("^[a-zA-Z0-9]+$").test(values.nameProd)) {
-      errors.nameProd = "Введите продукт кириллицей";
-    }
-    if (!values.gramProd) {
-      errors.gramProd = "Required";
-    }
-    if (values.gramProd && RegExp("^d+$").test(values.gramProd)) {
-      errors.gramProd = "Введите цифры";
-    }
-    return errors;
+import React, { useState } from "react";
+import AsyncSelect from "react-select/async";
+import axios from "axios";
+function AddProductForm() {
+  const [inputValue, setValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState(null);
+  // handle input change event
+  const handleInputChange = (value) => {
+    setValue(value);
   };
 
-  const { form, handleSubmit, values, pristine, submitting } = useForm({
-    onSubmit,
-    validate,
-  });
+  // handle selection
+  const handleChange = (value) => {
+    setSelectedValue(value);
+  };
 
-  const nameProd = useField("nameProd", form);
-  const gramProd = useField("gramProd", form);
+  // load options using API call
+  const loadOptions = (inputValue) => {
+    let sel;
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .get(`https://slimmom.herokuapp.com/products?name=${inputValue}`, {
+        headers,
+      })
+      .then((response) => {
+        const options = response.data.docs;
+        sel = options.map((item) => item.title.ru);
+        console.log(sel);
+      })
+      .catch((error) => {
+        if (error) {
+          console.log("its some errors ", error);
+        }
+      });
+    return sel;
+  };
 
-  const currentHideNav = useMediaQuery("(min-width: 767px)");
   return (
-    <>
-      <Calendar></Calendar>
-      <form onSubmit={handleSubmit} className={`${styles.ProductEditor} `}>
-        <label className={`${styles.ProductEditorLabel} `}>
-          <input
-            name="nameProd"
-            {...nameProd.input}
-            className={`${styles.ProductEditorInput} ${styles.ProductEditorInputName}`}
-            type="text"
-            placeholder="Введите название продукта*"
-          />
-          {nameProd.meta.error && nameProd.meta.touched && (
-            <span className={`${styles.ProductEditorErrorMsg}`}>
-              {nameProd.meta.error}
-            </span>
-          )}
-        </label>
-
-        <label className={`${styles.ProductEditorLabel} ${styles.Otstup}`}>
-          <input
-            name="gramProd"
-            {...gramProd.input}
-            className={`${styles.ProductEditorInput}  ${styles.ProductEditorInputKkal}`}
-            type="number"
-            placeholder="Граммы*"
-          />
-          {gramProd.meta.error && gramProd.meta.touched && (
-            <span className={`${styles.ProductEditorErrorMsg}`}>
-              {gramProd.meta.error}
-            </span>
-          )}
-        </label>
-
-        <button
-          type="submit"
-          disabled={pristine || submitting}
-          className={styles.ProductEditorButton}
-        >
-          {currentHideNav ? "+" : "Добавить"}
-        </button>
-      </form>
-
-      <DiaryProductList></DiaryProductList>
-    </>
+    <div className="App">
+      <pre>Input Value: "{inputValue}"</pre>
+      <AsyncSelect
+        cacheOptions
+        defaultOptions
+        value={selectedValue}
+        getOptionLabel={(e) => e.title}
+        getOptionValue={(e) => e.id}
+        loadOptions={loadOptions}
+        onInputChange={handleInputChange}
+        onChange={handleChange}
+      />
+      <pre>Selected Value: {JSON.stringify(selectedValue || {}, null, 2)}</pre>
+    </div>
   );
-};
+}
+
 export default AddProductForm;
