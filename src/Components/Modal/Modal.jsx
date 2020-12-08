28c5c7afbe0d009a503b6b38a2adaef3337eb4e8
unknown <you@example.com>
2020-12-08 22:41:50 +0200

    fixed modal

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
-
 import DailyCaloriesIntake from "../DailyCaloriesIntake";
 import Spiner from "../Spiner";
 import styles from "./Modal.module.css";
 
 const Modal = ({ isShowing, hide, calories, list }) => {
+  useEffect(() => {
+    function onKeyup(e) {
+      if (e.keyCode === 27) {
+        if (isShowing) {
+          hide();
+        }
+      }
+    }
+    window.addEventListener("keyup", onKeyup);
+    return () => window.removeEventListener("keyup", onKeyup);
+  }, [isShowing, hide]);
+
   return (
     <>
       {isShowing
